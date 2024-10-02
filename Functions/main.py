cbeae9ca24370b866f2faed38fd39d59cd6b1400
zuLton <zuLton@JOHLTY>
2024-10-02 15:04:43 +0300

    final commit

--- Functions/main.py
+++ Functions/main.py
@@ -1,14 +1,9 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from Interface.input_handler import get_user_input
 from Functions.art_generator import generate_ascii_art
 from Interface.output_formatter import format_output
 from FileHandler.file_handler import save_to_file
-from Functions.font_selector import choose_font  
-from Functions.color_selector import choose_color  
+from Functions.font_selector import choose_font
+from Functions.color_selector import choose_color
 from colorama import init
 
 init(autoreset=True)
@@ -28,6 +23,3 @@ def main():
         if restart_choice != 'y':
             print("Дякую за використання програми!")
             break
-
-if __name__ == "__main__":
-    main()
